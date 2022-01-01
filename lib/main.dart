e197e5bb211af8f6d8cf2aa8a4a1ece9673992b7
Paarthbathla338 <Paarth.bathla@outlook.com>
2022-01-01 23:06:15 +0530

    Infinite Scrollinng View Added

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,50 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Startup Name Generator'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+class RandomWords extends StatefulWidget {
+  const RandomWords({ Key? key }) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildSuggestions(),
+    );
+    
+
   }
 }
